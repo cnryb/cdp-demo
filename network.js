3fdd148f83133cfa3f850044681f0965721cd822
任跃兵 <cnryb@live.cn>
2022-04-21 10:28:06 +0800

    代码格式化

--- network.js
+++ network.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    headless: false, 
+    headless: false,
     devtools: true,
     args: ['--remote-debugging-port=9222']
   });
@@ -15,40 +15,42 @@ const puppeteer = require('puppeteer');
   const client = await CDP({ host: '127.0.0.1', port: 9222 });
   // extract domains
   const { Network, Page } = client;
-  
+
   // enable events then start!
   await Network.enable();
   // await Network.setBlockedURLs({ urls: ['**/*.png'] })
 
-  await Network.setRequestInterception({ patterns: [{ urlPattern: '*.js*', resourceType: 'Script', interceptionStage: 'HeadersReceived' },{urlPattern:'**/*.png*'}] });
-  await  Network.requestIntercepted(async (params) => {
-    console.log(params)
+  await Network.setRequestInterception({
+    patterns: [
+      { urlPattern: '*.js*', resourceType: 'Script', interceptionStage: 'HeadersReceived' },
+      { urlPattern: '**/*.png*' }]
+  });
+  await Network.requestIntercepted(async (params) => {
     if (params.request.url === 'https://fecdn3.zhaopin.cn/front-end/assets/logo-home.e56618.png?x-oss-process=image/resize,m_lfit,w_400') {
       await Network.continueInterceptedRequest({
         interceptionId: params.interceptionId,
         url: "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
       });
-    } else if(params.request.url === 'https://common-bucket.zhaopin.cn/js/zpfe-widget-sdk/zpfe-widget-sdk-1.0.0.js')
-    {
+    } else if (params.request.url === 'https://common-bucket.zhaopin.cn/js/zpfe-widget-sdk/zpfe-widget-sdk-1.0.0.js') {
       const response = await Network.getResponseBodyForInterception({ interceptionId: params.interceptionId });
-      const bodyData = response.base64Encoded ? Buffer.from(response.body,'base64').toString() : response.body;
-  
+      const bodyData = response.base64Encoded ? Buffer.from(response.body, 'base64').toString() : response.body;
+
       const newBody = bodyData + `\nconsole.error('Executed modified resource for ${params.request.url}');
       //console.error('~~~~~~~~~~~~~~~~');
       `;
-  
+
       const newHeaders = [
         'Connection: closed',
         'Content-Length: ' + newBody.length,
         'Content-Type: text/javascript'
       ];
-  
+
       await Network.continueInterceptedRequest({
         interceptionId: params.interceptionId,
-        rawResponse: Buffer.from('HTTP/1.1 200 OK' + '\r\n' + newHeaders.join('\r\n') + '\r\n\r\n' + newBody,'utf8').toString('base64')
+        rawResponse: Buffer.from('HTTP/1.1 200 OK' + '\r\n' + newHeaders.join('\r\n') + '\r\n\r\n' + newBody, 'utf8').toString('base64')
       });
-    }else{
-     await Network.continueInterceptedRequest({
+    } else {
+      await Network.continueInterceptedRequest({
         interceptionId: params.interceptionId
       });
     }
